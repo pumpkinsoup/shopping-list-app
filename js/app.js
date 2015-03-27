05a5bf25cae3ac191f2036c0ea2f03bac2984706
joni <sanapakaininmoko@yahoo.com>
2015-03-27 15:15:38 -0400

    pushing shopping list

--- js/app.js
+++ js/app.js
@@ -2,11 +2,15 @@ $(document).ready(function () {
 
     $('#addItem').on('click', function () {
         var item = $('#myInput').val();
-        $('#list').append('<li class="item">' + item + '</li>');
+        $('#list').append('<li class="item">' + item + '<span class="delete">click here to delete</span></li>');
     })
 
     $(document).on('click', '.item', function () {
         $(this).toggleClass('purchased');
+    })
+
+    $(document).on('dblclick', '.item', function () {
+    	$(this).remove();
     })
 
 })
